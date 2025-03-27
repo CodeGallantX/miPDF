3b9e67fb9040c53ccfff3261ccf4afc9ad5e93c7
John A. Samuel <johnayobami77@proton.me>
2025-03-27 03:09:40 +0100

    Increased dduration for tokens

--- miPDF/settings.py
+++ miPDF/settings.py
@@ -83,6 +83,15 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60), 
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7), 
+    'ROTATE_REFRESH_TOKENS': True,             
+    'BLACKLIST_AFTER_ROTATION': True,          
+}
+
+
 ROOT_URLCONF = 'miPDF.urls'
 
 TEMPLATES = [
